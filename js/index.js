8ad1061ee9e36c245c23f8466ce82f2aad40aa35
E200025 <n.berger@student.helmo.be>
2022-05-20 10:41:41 +0200

    Changing hostname to href in order

--- js/index.js
+++ js/index.js
@@ -6,13 +6,13 @@ const sha = '3c1b383b943353773a747d678f40e8d127c4130c4da68c953bb92524b2c4cfbc'
 button.addEventListener('click', () => {
     password_input.classList.remove('is-invalid')
     const password = password_input.value
-    hostname = window.location.hostname;
+    href = window.location.href;
     if (password) {
         if (password === 'MesPronos') {
-            window.location.replace(`${hostname}pronosics.html`)
+            window.location.replace(`${href}pronosics.html`)
         }
         if (sha256(password) === sha) {
-            window.location.replace(`${hostname}pronosics.html`)
+            window.location.replace(`${href}pronosics.html`)
         }
     }
     password_input.classList.add('is-invalid')
