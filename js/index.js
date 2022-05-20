b3c73191ed8adb3ea3c6a7130db7d0ba060d7598
E200025 <n.berger@student.helmo.be>
2022-05-20 10:38:43 +0200

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+let button = document.getElementById('enter_btn')
+let password_input = document.getElementById('password')
+
+const sha = '3c1b383b943353773a747d678f40e8d127c4130c4da68c953bb92524b2c4cfbc'
+
+button.addEventListener('click', () => {
+    password_input.classList.remove('is-invalid')
+    const password = password_input.value
+    hostname = window.location.hostname;
+    if (password) {
+        if (password === 'MesPronos') {
+            window.location.replace(`${hostname}pronosics.html`)
+        }
+        if (sha256(password) === sha) {
+            window.location.replace(`${hostname}pronosics.html`)
+        }
+    }
+    password_input.classList.add('is-invalid')
+})
