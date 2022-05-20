bbb69446d67507ef24ce4a15c215a1a42bb7ad20
E200025 <n.berger@student.helmo.be>
2022-05-20 10:42:48 +0200

    Correction error

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const sha = '3c1b383b943353773a747d678f40e8d127c4130c4da68c953bb92524b2c4cfbc'
 button.addEventListener('click', () => {
     password_input.classList.remove('is-invalid')
     const password = password_input.value
-    href = window.location.href;
+    let href = window.location.href;
     if (password) {
         if (password === 'MesPronos') {
             window.location.replace(`${href}pronosics.html`)
